5374858c44a9d8214c4f6616b464b1906a54478d
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-01-27 21:00:58 +0530

    Made t shirts page get all the t shirts

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,100 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
 
-const tshirts = () => {
+const tshirts = (props) => {
+
     return (
         <div className="p-4">
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 w-fit mx-auto">
                     <div className="flex flex-wrap -m-4 justify-center p-6">
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
-                        <Link href="/product/t-shirt" className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1">
-                                <div className="block relative h-48 rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-60 w-72 md:w-60 mx-auto " src="https://m.media-amazon.com/images/I/51byp5tQ86L._SX679_.jpg" />
-                                </div>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">Blue T shirt</h2>
-                                    <p className="mt-1">₹899</p>
-                                    <p className="mt-1">Sizes: M, XL, XXL</p>
-                                </div>  
-                        </Link>
+                        {
+                            props.products.map((product, key) => (
+                                <Link href={`/product/${product.slug}`} className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-md rounded-lg m-1" key={key}>
+                                    <div className="block relative h-48 rounded overflow-hidden">
+                                        <img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] w-[30rem] md:w-[40rem]" src={product.img} />
+                                    </div>
+                                    <div className="mt-4 text-center md:text-left">
+                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{product.category}</h3>
+                                        <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
+                                        <p className="mt-1">₹{product.price}</p>
+                                        <p className="mt-1">Sizes: {product.size}</p>
+                                    </div>
+                                </Link>
+                            ))
+                        }
                     </div>
                 </div>
             </section>
@@ -102,4 +30,16 @@ const tshirts = () => {
     )
 }
 
-export default tshirts
+export async function getServerSideProps() {
+    const res = await fetch(`http://localhost:3000/api/getproducts`);
+    const jsonData = await res.json();
+    jsonData.products = jsonData.products.filter((product) => product.category === "T Shirts");
+
+    return {    
+        props: {
+            products: jsonData.products
+        }
+    }
+};
+
+export default tshirts;
