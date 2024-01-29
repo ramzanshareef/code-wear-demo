44cddb1c2efefb7520b89a102aaa9dfb5a6d0b05
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-01-29 19:06:25 +0530

    Add razorpay payment integration

---
+++ pages/api/handlePayment.js
@@ -0,0 +1,24 @@
+import Razorpay from "razorpay";
+
+export default function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+    var instance = new Razorpay({
+        key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+    const { amount } = req.body;
+    var options = {
+        amount: amount*100,
+        currency: "INR",
+    };
+    instance.orders.create(options, function (err, order) {
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
+        return res.status(200).json({
+            order: order,
+        });
+    });
+}
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,16 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { IoRemoveCircleOutline, IoAddCircleOutline } from "react-icons/io5";
 import { BsFillBagCheckFill } from "react-icons/bs";
 import Head from "next/head";
 import Script from "next/script";
+import { useRouter } from "next/router";
 
 const checkout = (props) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phoneno, setPhoneno] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [pincode, setPincode] = useState("");
+    // const [paymentDisabled, setPaymentDisabled] = useState(true);
+    const router = useRouter();
+
     const initiatePayment = async (e) => {
         e.preventDefault();
-        alert("Payment done");
+        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/handlePayment`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                amount: props.subTotal,
+            }),
+        });
+        if (response.status === 200) {
+            const data = await response.json();
+            var options = {
+                "key": process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+                "amount": data.order.amount,
+                "currency": "INR",
+                "name": "Code Wear",
+                "description": "Test Transaction",
+                "image": "https://scontent.fvga2-1.fna.fbcdn.net/v/t39.30808-6/333269272_729527645556981_8288775143650171103_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=OMfzubTugDAAX88YZIl&_nc_ht=scontent.fvga2-1.fna&oh=00_AfAbys8Jry1G7eNicpd5AkVK80Esu_daSF-zFOGssNIkbw&oe=65BC80FE",
+                "order_id": data.order.id,
+                "handler": function (response) {
+                    console.log("Payment Successful")
+                    console.log(response)
+                },
+                "prefill": {
+                    "name": "Gaurav Kumar",
+                    "email": "gaurav.kumar@example.com",
+                    "contact": "9000090000"
+                },
+                "notes": {
+                    "address": "Razorpay Corporate Office"
+                },
+                "theme": {
+                    "color": "#3399cc"
+                }
+            };
+            function loadScript(src) {
+                return new Promise((resolve) => {
+                    const script = document.createElement("script");
+                    script.src = src;
+                    script.onload = () => {
+                        resolve(true);
+                    };
+                    script.onerror = () => {
+                        resolve(false);
+                    };
+                    document.body.appendChild(script);
+                });
+            }
+            var res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+            if (!res) {
+                alert("Razorpay SDK failed to load. Are you online?");
+                return;
+            }
+            else {
+                var rzp1 = new Razorpay(options);
+                rzp1.open();
+                rzp1.on('payment.failed', function (response) {
+                    console.log(response)
+                });
+            }
+
+        }
     }
 
+    // useEffect(() => {
+    //     if (name.length > 0 && email.length>0 && address.length > 0 && phoneno.length ===10 && city.length > 0 && state.length > 0 && pincode.length === 6) {
+    //         setPaymentDisabled(false);
+    //     }
+    //     else {
+    //         setPaymentDisabled(true);
+    //     }
+    // }, [name, email, address, phoneno, city, state, pincode])
 
 
     return (
@@ -18,9 +98,6 @@ const checkout = (props) => {
             <Head>
                 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" />
             </Head>
-            <Script>
-                <Script src="https://checkout.razorpay.com/v1/checkout.js"></Script>
-            </Script>
             <div className="container my-8 w-4/5 mx-auto">
                 <form>
                     <h1 className="w-fit mx-auto text-2xl font-bold">
@@ -37,12 +114,24 @@ const checkout = (props) => {
                                         <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
                                         <input type="text" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                             required
+                                            value={name}
+                                            onChange={
+                                                (e) => {
+                                                    setName(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                     <div className="w-full md:w-1/2">
                                         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
                                         <input type="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                             required
+                                            value={email}
+                                            onChange={
+                                                (e) => {
+                                                    setEmail(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                 </div>
@@ -50,6 +139,13 @@ const checkout = (props) => {
                                     <label htmlFor="address" className="leading-7 text-sm text-gray-600">Address</label>
                                     <textarea name="address" cols="30" rows="2"
                                         className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                        required
+                                        value={address}
+                                        onChange={
+                                            (e) => {
+                                                setAddress(e.target.value);
+                                            }
+                                        }
                                     ></textarea>
                                 </div>
                                 <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:justify-between my-2">
@@ -59,24 +155,50 @@ const checkout = (props) => {
                                             required
                                             maxLength={10}
                                             minLength={10}
+                                            value={phoneno}
+                                            onChange={
+                                                (e) => {
+                                                    setPhoneno(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                     <div className="w-full md:w-1/5">
                                         <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
                                         <input type="text" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                             required
+                                            value={city}
+                                            onChange={
+                                                (e) => {
+                                                    setCity(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                     <div className="w-full md:w-1/5">
                                         <label htmlFor="city" className="leading-7 text-sm text-gray-600">State</label>
                                         <input type="text" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                             required
+                                            value={state}
+                                            onChange={
+                                                (e) => {
+                                                    setState(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                     <div className="w-full md:w-1/5">
                                         <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">Pin Code</label>
                                         <input type="number" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                             required
+                                            maxLength={6}
+                                            minLength={6}
+                                            value={pincode}
+                                            onChange={
+                                                (e) => {
+                                                    setPincode(e.target.value);
+                                                }
+                                            }
                                         />
                                     </div>
                                 </div>
@@ -131,8 +253,9 @@ const checkout = (props) => {
                                 </div>
                             </div>
 
-                            <button type="submit" className="text-white bg-blue-500 border-0 p-2 focus:outline-none hover:bg-blue-600 rounded w-fit my-4"
-                            onClick={initiatePayment}
+                            <button type="submit" className="text-white bg-blue-500 border-0 p-2 focus:outline-none hover:bg-blue-600 rounded w-fit my-4 disabled:cursor-not-allowed disabled:bg-blue-400"
+                                onClick={initiatePayment}
+                            // disabled={paymentDisabled}
                             >
                                 <div className="flex flex-row items-center justify-center space-x-1">
                                     <BsFillBagCheckFill className="text-xl" />
